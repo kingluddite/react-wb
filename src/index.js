08f5b41b62951bb0873652b973477f62a5f431f6
pip_lineupbuilder <howley.phil@gmail.com>
2017-02-27 23:27:50 -0800

    Save local changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const repo = `/${window.location.pathname.split('/')[1]}`;
 
 const Root = () => {
   return (
-    <BrowserRouter basename="/teamsanity/">
+    <BrowserRouter basename={repo}>
       <div>
         <Match exactly pattern="/" component={TeamPicker} />
         <Match pattern="/team/:teamId" component={App} />
